d4501a0cbdf7d4560f1b197b4052a91102445e56
venati sasi <sasirekha.ca22@gmail.com>
2023-01-25 11:46:53 +0530

    modified

---
+++ Components/Child.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Child(){
+    return(
+        <div>
+            <h1>sasi</h1>
+            
+        </div>
+    )
+}
+export default Child;
---
+++ Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Navbar extends React.Component{
+render(){
+    return(
+        <div>
+            <h1>class based componet</h1>
+        </div>
+    )
+}
+
+}
+export default Navbar;
